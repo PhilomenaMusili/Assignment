6e2bae15117fe3f297ede8c44752925e0342d6c7
PhilomenaMusili <philomenamusili2@gmail.com>
2024-07-08 18:59:56 +0300

    Solution

---
+++ ass1.py
@@ -0,0 +1,15 @@
+# User inputs
+one= int(input('enter First Number: '))
+two = int(input('enter Second Number: '))
+
+# Instructions
+sum = one + two
+product = one * two
+division = one / two
+diff = one - two
+
+# Output
+print(f"The sum of {one} and {two} is: {sum}")
+print(f"The Product of {one} and {two} is: {product}")
+print(f"The Division of {one} and {two} is: {division}")
+print(f"The Diffrence of {one} and {two} is: {diff}")
